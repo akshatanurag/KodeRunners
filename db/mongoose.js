bd597c404c6413f006bc9197630cefe70bf0c9a7
Sanat B Singh <1705068@kiit.ac.in>
2018-10-06 07:19:57 +0530

    db

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/kodeRunners",()=>{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://x2799:krcms1@ds137600.mlab.com:37600/koderunners",()=>{
     console.log("connected to db");
 });
 mongoose.Promise = global.Promise;
